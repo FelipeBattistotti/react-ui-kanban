f2ae5d42146d89d2cb802d0276d666ff5ec4c889
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-09-27 14:08:42 -0300

    first commit

---
+++ src/locales/index.js
@@ -0,0 +1,13 @@
+// i18next support structure
+
+export default {
+  "en": {
+    translation: require('./en/translation.json')
+  },
+  "ru": {
+    translation: require('./ru/translation.json')
+  },
+  "pt-br": {
+    translation: require('./pt-br/translation.json')
+  }
+}
